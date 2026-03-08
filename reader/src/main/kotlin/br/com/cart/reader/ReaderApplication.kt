0bab436d8787888c4d752ce1cf5bcf9d2ca37246
GuiRL-Dev <guirl.dev@gmail.com>
2026-03-08 20:02:50 -0300

    add reader mc + add init sql and change docker compose

---
+++ reader/src/main/kotlin/br/com/cart/reader/ReaderApplication.kt
@@ -0,0 +1,11 @@
+package br.com.cart.reader
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReaderApplication
+
+fun main(args: Array<String>) {
+	runApplication<ReaderApplication>(*args)
+}
